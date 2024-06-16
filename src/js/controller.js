8f8186727032f9bb629146ec79c72ff3f826bb69
Manthan Kaneriya <kaneriyamanthan@gmail.com>
2024-06-16 09:22:08 +0530

    Refactoerd the code using MVC architecture

---
+++ src/js/controller.js
@@ -0,0 +1,41 @@
+import * as model from "./model.js";
+import recipeView from "./views/recipeView.js";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+const recipeContainer = document.querySelector(".recipe");
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+// https://forkify-api.herokuapp.com/v2
+
+///////////////////////////////////////
+
+const controllerRecipe = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
+
+    recipeView.renderSpinner(recipeContainer);
+
+    // 1. Loading recipe
+    await model.loadRecipe(id);
+    const { recipe } = model.state;
+
+    //2. Rendering Recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+["hashchange", "load"].forEach((ev) =>
+  window.addEventListener(ev, controllerRecipe)
+);
