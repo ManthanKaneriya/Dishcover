bdee7a7575e5b2235872715e8f5185b341df2101
Manthan Kaneriya <kaneriyamanthan@gmail.com>
2024-06-19 22:09:01 +0530

    Implemented update servings functionality on Dishcover Web App

--- src/js/model.js
+++ src/js/model.js
@@ -61,3 +61,12 @@ export const getSearchResultsPage = function (page = 1) {
 
   return state.search.results.slice(start, end);
 };
+
+export const updateServings = function (newServings) {
+  state.recipe.ingredients.forEach((ing) => {
+    ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
+  });
+
+  state.recipe.servings = newServings;
+  console.log(newServings);
+};
