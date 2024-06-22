bb2dc84a7ca6c10bbb95e5c017a89455b273242b
Manthan Kaneriya <kaneriyamanthan@gmail.com>
2024-06-22 15:02:22 +0530

    Made it deploy ready

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,5 @@
 import View from "./View";
 import icons from "../../img/icons.svg";
-import { Fraction } from "fractional";
 
 class RecipeView extends View {
   _parentElement = document.querySelector(".recipe");
@@ -129,7 +128,7 @@ class RecipeView extends View {
                 <use href="${icons}#icon-check"></use>
               </svg>
               <div class="recipe__quantity">${
-                ing.quantity ? new Fraction(ing.quantity).toString() : ""
+                ing.quantity ? ing.quantity : ""
               }</div>
               <div class="recipe__description">
                 <span class="recipe__unit">${ing.unit}</span>
